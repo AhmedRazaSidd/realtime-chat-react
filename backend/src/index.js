75efda83be1e29a4b75ad895f028cb6c03139950
AhmedRazaSidd <ar5812264@gmail.com>
2025-05-18 10:53:05 -0700

    env

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,11 +12,11 @@ import { app, server, io } from './lib/socket.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ limit: "5mb", extended: true }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));  
 app.use(cookiesParser());
 app.use(cors({
     origin: 'https://realtime-chat-react.vercel.app',
