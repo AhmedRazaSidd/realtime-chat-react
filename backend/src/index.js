5f7832bef49dc32f39e51596033a3fc649346ca6
AhmedRazaSidd <ar5812264@gmail.com>
2025-05-18 12:21:05 -0700

    localhost change

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import cors from 'cors';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
-import { app, server, io } from './lib/socket.js';
+import { app, server } from './lib/socket.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
