cc89a8796a40f733d28b7d67e93bb1105ef4aa6f
AhmedRazaSidd <ar5812264@gmail.com>
2025-05-18 12:40:50 -0700

    Add ifi

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import cookiesParser from 'cookie-parser';
 import cors from 'cors';
+
+// import path from 'path'
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -10,31 +12,41 @@ import { app, server } from './lib/socket.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+// const __dirname = path.resolve();
 
-// Database Connection
 (async () => {
     try {
+        // Connect to the Database
         await connectDB();
-
         // Middleware
         app.use(express.json({ limit: "5mb" }));
-        app.use(express.urlencoded({ limit: "5mb", extended: true }));  
-        app.use(cookieParser());
+        app.use(express.urlencoded({ limit: "5mb", extended: true }));
+        app.use(cookiesParser());
         app.use(cors({
-            origin: process.env.FRONTEND_URL || 'https://realtime-chat-react.vercel.app',
+            origin: 'https://realtime-chat-react.vercel.app',
             credentials: true,
         }));
+
 
         // Routes
         app.use('/api/auth', authRoutes);
         app.use('/api/messages', messageRoutes);
 
-        // Server Start
-        server.listen(PORT, () => {
+        // if (process.env.NODE_ENV==='production') {
+        //     app.use(express.static(path.json(__dirname,'../frontend/public/dist')));
+
+        //     app.get('*',(req,res)=>{
+        //         res.sendFile(path.join(__dirname,'../frontend/public','dist','index.html'));
+        //     })
+        // }
+
+        // Start Server
+        server.listen(PORT, async () => {
             console.log('====================================');
             console.log(`Server is Running on Port ${PORT}`);
             console.log('====================================');
         });
+
     } catch (error) {
         console.error('Failed to connect to MongoDB:', error.message);
         process.exit(1);
